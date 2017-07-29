c35bb9c51b2a67dc043b6e0903b5432d7e73dfe3
Yakka <Yakka@users.noreply.github.com>
2017-07-29 10:42:09 +0200

    First debug view

--- HarderStronger/Assets/Scripts/Dick.cs
+++ HarderStronger/Assets/Scripts/Dick.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class Dick : MonoBehaviour {
     
     public Section sectionModel = null;
+    public int amountOfSections = 5;
+    public int gapBetweenTwoSections = 10;
+
+    private List<Section> sectionsList = new List<Section>();
 
 	void Start () {
 		if(sectionModel != null) {
-            Instantiate(sectionModel);
+            for(int i = 0; i < amountOfSections; i++) {
+                Section section = Instantiate(sectionModel);
+                section.transform.parent = this.transform;
+                section.transform.Translate(Vector3.left * i * gapBetweenTwoSections);
+                section.name = "Section (" + i + ")";
+
+                sectionsList.Add(section);
+            }
         } else {
             Debug.Log("Error: no section model, cannot instantiate the dick.");
         }
 	}
 	
 	void Update () {
-		
+        Vector3 previousPosition = Vector3.zero;
+		foreach(Section section in sectionsList) {
+            Debug.DrawLine(previousPosition, section.transform.position);
+            previousPosition = section.transform.position;
+        }
 	}
 }
