ddfc33d56bd710089bc95b14c88bf861c4f41a60
Yakka <Yakka@users.noreply.github.com>
2017-07-29 10:22:40 +0200

    First dick scripts

---
+++ HarderStronger/Assets/Scripts/Dick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dick : MonoBehaviour {
+    
+    public Section sectionModel = null;
+
+	void Start () {
+		if(sectionModel != null) {
+            Instantiate(sectionModel);
+        } else {
+            Debug.Log("Error: no section model, cannot instantiate the dick.");
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ HarderStronger/Assets/Scripts/Section.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour {
+
+    public Vector2 topVertice, bottomVertice;
+    public float angle = 0;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
