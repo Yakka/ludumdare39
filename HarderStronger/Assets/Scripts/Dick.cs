2397e22351f4e404774d59756e8992a7d9255a50
Yakka <Yakka@users.noreply.github.com>
2017-07-29 16:24:04 +0200

    Generated a mesh.

--- HarderStronger/Assets/Scripts/Dick.cs
+++ HarderStronger/Assets/Scripts/Dick.cs
@@ -11,11 +11,44 @@ public class Dick : MonoBehaviour {
     private List<Section> sectionsList = new List<Section>();
     private int selectedSectionID = 0;
 
-	void Start () {
+    private List<Vector3> verticesList = new List<Vector3>();
+    private List<int> trianglesList = new List<int>();
+    private List<Vector3> normalsList = new List<Vector3>();
+
+    void Start () {
 		if(sectionModel != null) {
-            for(int i = 0; i < amountOfSections; i++) {
+            Mesh mesh = new Mesh();
+
+            GetComponent<MeshFilter>().mesh = mesh;
+
+            verticesList.Add(Vector3.up);
+            verticesList.Add(Vector3.down);
+            verticesList.Add(Vector3.left);
+            verticesList.Add(Vector3.right);
+
+            mesh.vertices = verticesList.ToArray();
+
+            trianglesList.Add(0);
+            trianglesList.Add(1);
+            trianglesList.Add(2);
+
+            trianglesList.Add(1);
+            trianglesList.Add(0);
+            trianglesList.Add(3);
+
+            mesh.triangles = trianglesList.ToArray();
+
+            for(int i = 0; i < verticesList.Count; i++) {
+                normalsList.Add(Vector3.back);
+            }
+
+            mesh.normals = normalsList.ToArray();
+
+            mesh.uv = new Vector2[]{ Vector2.zero, Vector2.right, Vector2.up, Vector2.right + Vector2.up };
+
+            for (int i = 0; i < amountOfSections; i++) {
                 // Setting the new game object
-                Section section = Instantiate(sectionModel);
+                /*Section section = Instantiate(sectionModel);
                 section.transform.parent = this.transform;
                 section.name = "Section (" + i + ")";
                 // Setting its coordinates
@@ -28,7 +61,9 @@ public class Dick : MonoBehaviour {
                     section.bottomVertice = section.pivot - Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
                 }
                 // Adding to the list
-                sectionsList.Add(section);
+                sectionsList.Add(section);*/
+
+                
             }
         } else {
             Debug.Log("Error: no section model, cannot instantiate the dick.");
