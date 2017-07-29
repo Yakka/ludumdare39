cc0808fdb1975d08195eda9f3cdb10fae4800c93
Yakka <Yakka@users.noreply.github.com>
2017-07-29 17:30:00 +0200

    Full mesh with texture.

--- HarderStronger/Assets/Scripts/Dick.cs
+++ HarderStronger/Assets/Scripts/Dick.cs
@@ -14,57 +14,64 @@ public class Dick : MonoBehaviour {
     private List<Vector3> verticesList = new List<Vector3>();
     private List<int> trianglesList = new List<int>();
     private List<Vector3> normalsList = new List<Vector3>();
+    private List<Vector2> uvsList = new List<Vector2>();
 
     void Start () {
 		if(sectionModel != null) {
-            Mesh mesh = new Mesh();
-
-            GetComponent<MeshFilter>().mesh = mesh;
-
-            verticesList.Add(Vector3.up);
-            verticesList.Add(Vector3.down);
-            verticesList.Add(Vector3.left);
-            verticesList.Add(Vector3.right);
-
-            mesh.vertices = verticesList.ToArray();
-
-            trianglesList.Add(0);
-            trianglesList.Add(1);
-            trianglesList.Add(2);
-
-            trianglesList.Add(1);
-            trianglesList.Add(0);
-            trianglesList.Add(3);
-
-            mesh.triangles = trianglesList.ToArray();
-
-            for(int i = 0; i < verticesList.Count; i++) {
-                normalsList.Add(Vector3.back);
-            }
-
-            mesh.normals = normalsList.ToArray();
-
-            mesh.uv = new Vector2[]{ Vector2.zero, Vector2.right, Vector2.up, Vector2.right + Vector2.up };
-
             for (int i = 0; i < amountOfSections; i++) {
                 // Setting the new game object
-                /*Section section = Instantiate(sectionModel);
+                Section section = Instantiate(sectionModel);
                 section.transform.parent = this.transform;
                 section.name = "Section (" + i + ")";
                 // Setting its coordinates
-                if (i > 0) {
+                /*if (i > 0) {
                     section.eulerAngle = Vector3.back * 30;
                     section.pivot = sectionsList[i - 1].pivot 
                         + gapBetweenTwoSections * Mathf.Cos(sectionsList[i - 1].eulerAngle.magnitude) * Vector3.left
                         - gapBetweenTwoSections * Mathf.Sin(sectionsList[i - 1].eulerAngle.magnitude) * Vector3.up;
                     section.topVertice = section.pivot + Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
                     section.bottomVertice = section.pivot - Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
-                }
+                }*/
                 // Adding to the list
-                sectionsList.Add(section);*/
-
-                
+                sectionsList.Add(section);
             }
+            // Generating the mesh
+            Mesh mesh = new Mesh();
+
+            GetComponent<MeshFilter>().mesh = mesh;
+            
+
+            for (int i = 0; i < amountOfSections; i++) {
+                // Upper vertice
+                verticesList.Add(Vector3.up + Vector3.left * i);
+                uvsList.Add(new Vector2(amountOfSections - i, 1));
+                normalsList.Add(Vector3.back);
+
+                // Lower vertice
+                verticesList.Add(Vector3.down + Vector3.left * i);
+                uvsList.Add(new Vector2(amountOfSections - i, -1));
+                normalsList.Add(Vector3.back);
+
+                // Triangles
+                if(i > 0) {
+                    trianglesList.Add((i * 2) - 2);
+                    trianglesList.Add((i * 2) - 1);
+                    trianglesList.Add(i * 2);
+
+                    trianglesList.Add((i * 2) - 1);
+                    trianglesList.Add((i * 2) + 1);
+                    trianglesList.Add(i * 2);
+                }
+            }
+
+            // Building the mesh
+            mesh.Clear();
+
+            mesh.vertices = verticesList.ToArray();
+            mesh.triangles = trianglesList.ToArray();
+
+            mesh.normals = normalsList.ToArray();
+            mesh.uv = uvsList.ToArray();
         } else {
             Debug.Log("Error: no section model, cannot instantiate the dick.");
         }
