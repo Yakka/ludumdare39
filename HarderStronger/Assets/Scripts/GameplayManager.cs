30205ca57f18efbb0ffb16dff06e492bda0e7957
Yakka <Yakka@users.noreply.github.com>
2017-07-29 13:46:02 +0200

    Power is connected to gameplay.

--- HarderStronger/Assets/Scripts/GameplayManager.cs
+++ HarderStronger/Assets/Scripts/GameplayManager.cs
@@ -7,11 +7,15 @@ public class GameplayManager : MonoBehaviour {
     static private GameplayManager instance = null;
 
     public Dick dick = null;
-    public int power = 100;
+    public int power;
+    public const int powerCost = 10;
+    public const int powerRefund = 5;
+    public const int powerMax = 100;
 
     // Use this for initialization
     void Start() {
         instance = this;
+        power = powerMax;
         if (dick == null) {
             Debug.Log("Error: no dick found.");
         }
@@ -26,10 +30,14 @@ public class GameplayManager : MonoBehaviour {
             dick.SelectOnRight();
         }
         if (Input.GetKeyUp(KeyCode.UpArrow)) {
-            dick.EnlargeSelectedSection();
+            if(power >= powerCost) {
+                dick.EnlargeSelectedSection();
+                power -= 10;
+            }
         }
         if (Input.GetKeyUp(KeyCode.DownArrow)) {
             dick.ShrinkSelectedSection();
+            power = Mathf.Min(power + powerRefund, powerMax);
         }
     }
 
