27965f2c9e17db27ff904570e4426a1608f6d4a0
Yakka <Yakka@users.noreply.github.com>
2017-07-29 19:55:41 +0200

    Enlarging gets smoother

--- HarderStronger/Assets/Scripts/Dick.cs
+++ HarderStronger/Assets/Scripts/Dick.cs
@@ -16,54 +16,62 @@ public class Dick : MonoBehaviour {
     private List<int> trianglesList = new List<int>();
     private List<Vector3> normalsList = new List<Vector3>();
     private List<Vector2> uvsList = new List<Vector2>();
+    private List<Vector3> anglesList = new List<Vector3>();
+    private List<float> sizesList = new List<float>();
     private Mesh mesh = null;
 
     void Start () {
-            /*for (int i = 0; i < amountOfSections; i++) {
-                // Setting the new game object
-                Section section = Instantiate(sectionModel);
-                section.transform.parent = this.transform;
-                section.name = "Section (" + i + ")";
-                // Setting its coordinates
-                if (i > 0) {
-                    section.eulerAngle = Vector3.back * 30;
-                    section.pivot = sectionsList[i - 1].pivot 
-                        + gapBetweenTwoSections * Mathf.Cos(sectionsList[i - 1].eulerAngle.magnitude) * Vector3.left
-                        - gapBetweenTwoSections * Mathf.Sin(sectionsList[i - 1].eulerAngle.magnitude) * Vector3.up;
-                    section.topVertice = section.pivot + Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
-                    section.bottomVertice = section.pivot - Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
-                }
-                // Adding to the list
-                // sectionsList.Add(section);
-            }*/
-            // Generating the mesh
-            mesh = new Mesh();
-
-            GetComponent<MeshFilter>().mesh = mesh;
-
-            for (int i = 0; i < amountOfSections; i++) {
-                // Upper vertice
-                verticesList.Add(Vector3.up * initialHeight  / 2f + Vector3.left * i);
-                uvsList.Add(new Vector2(amountOfSections - i, initialHeight / 2f));
-                normalsList.Add(Vector3.back);
-
-                // Lower vertice
-                verticesList.Add(Vector3.down * initialHeight / 2f + Vector3.left * i);
-                uvsList.Add(new Vector2(amountOfSections - i, -initialHeight / 2f));
-                normalsList.Add(Vector3.back);
-
-                if(i > 0) {
-                    // Upper triangle
-                    trianglesList.Add((i * 2) - 2);
-                    trianglesList.Add((i * 2) - 1);
-                    trianglesList.Add(i * 2);
-
-                    // Lower triangle
-                    trianglesList.Add((i * 2) - 1);
-                    trianglesList.Add((i * 2) + 1);
-                    trianglesList.Add(i * 2);
-                }
+        /*for (int i = 0; i < amountOfSections; i++) {
+            // Setting the new game object
+            Section section = Instantiate(sectionModel);
+            section.transform.parent = this.transform;
+            section.name = "Section (" + i + ")";
+            // Setting its coordinates
+            if (i > 0) {
+                section.eulerAngle = Vector3.back * 30;
+                section.pivot = sectionsList[i - 1].pivot 
+                    + gapBetweenTwoSections * Mathf.Cos(sectionsList[i - 1].eulerAngle.magnitude) * Vector3.left
+                    - gapBetweenTwoSections * Mathf.Sin(sectionsList[i - 1].eulerAngle.magnitude) * Vector3.up;
+                section.topVertice = section.pivot + Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
+                section.bottomVertice = section.pivot - Vector3.Cross(Vector3.forward, section.pivot).normalized * section.height / 2f;
             }
+            // Adding to the list
+            // sectionsList.Add(section);
+        }*/
+        // Generating the mesh
+        mesh = new Mesh();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        for (int i = 0; i < amountOfSections; i++) {
+            // Pivots
+            anglesList.Add(Vector3.left);
+            sizesList.Add(initialHeight);
+
+            // Upper vertice
+            verticesList.Add(Vector3.Cross(Vector3.back, anglesList[selectedSectionID]).normalized * sizesList[selectedSectionID] / 2f
+            + Vector3.left * i); 
+            uvsList.Add(new Vector2(amountOfSections - i, initialHeight / 2f));
+            normalsList.Add(Vector3.back);
+
+            // Lower vertice
+            verticesList.Add(Vector3.Cross(Vector3.forward, anglesList[selectedSectionID]).normalized * sizesList[selectedSectionID] / 2f
+            + Vector3.left * i);
+            uvsList.Add(new Vector2(amountOfSections - i, -initialHeight / 2f));
+            normalsList.Add(Vector3.back); 
+
+            if (i > 0) {
+                // Upper triangle
+                trianglesList.Add((i * 2) - 2);
+                trianglesList.Add((i * 2) - 1);
+                trianglesList.Add(i * 2);
+
+                // Lower triangle
+                trianglesList.Add((i * 2) - 1);
+                trianglesList.Add((i * 2) + 1);
+                trianglesList.Add(i * 2);
+            }
+        }
 
         // Building the mesh
         UpdateDick();
@@ -85,10 +93,17 @@ public class Dick : MonoBehaviour {
     }
 
     public void EnlargeSelectedSection() {
-        verticesList[selectedSectionID * 2] += Vector3.up * sizeUp;
+        anglesList[selectedSectionID] += Vector3.up * 0.1f;
+        sizesList[selectedSectionID] += sizeUp;
+
+        float dot = Vector3.Dot(anglesList[selectedSectionID], Vector3.up);
+
+        verticesList[selectedSectionID * 2] = Vector3.Cross(Vector3.back, anglesList[selectedSectionID]).normalized * sizesList[selectedSectionID] / 2f
+            + Vector3.left * selectedSectionID;
         uvsList[selectedSectionID * 2] += Vector2.up * sizeUp;
 
-        verticesList[selectedSectionID * 2 + 1] += Vector3.down * sizeUp;
+        verticesList[selectedSectionID * 2 + 1] = Vector3.Cross(Vector3.forward, anglesList[selectedSectionID]).normalized * sizesList[selectedSectionID] / 2f
+            + Vector3.left * selectedSectionID;
         uvsList[selectedSectionID * 2 + 1] += Vector2.down * sizeUp;
 
         UpdateDick();
