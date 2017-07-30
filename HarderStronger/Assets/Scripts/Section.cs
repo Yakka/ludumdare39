609610cfb688fac8f5e24436b345f27f58c89f56
Yakka <Yakka@users.noreply.github.com>
2017-07-30 16:06:42 +0200

    Click moves the dick.

--- HarderStronger/Assets/Scripts/Section.cs
+++ HarderStronger/Assets/Scripts/Section.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Section : MonoBehaviour {
-
-    public Vector3 angle = Vector3.left;
-    public Vector3 maxAngle = (Vector3.up + Vector3.left).normalized;
+    
+    public const float maxAngle = 45f;
+    public const float angleBoost = 5f;
 
     // Use this for initialization
     void Start () {
@@ -14,24 +14,14 @@ public class Section : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-        if(Input.GetMouseButtonDown(0)) {
+        if(Input.GetMouseButtonDown(0) && hit.collider != null) {
             if (hit.collider.name == name) {
-                Debug.Log(hit.collider.GetComponent<Section>().name);
+                BoostAngle(angleBoost);
             }
         }
-
-        transform.eulerAngles = angle;
     }
 
-    public void BoostAngle(Vector3 _angleBoost) {
-        angle += _angleBoost;
-        angle.Normalize();
-        if(Vector3.Angle(Vector3.left, angle) > Vector3.Angle(Vector3.left, _angleBoost)) {
-            if(angle.y > 0) {
-                angle = maxAngle;
-            } else {
-                angle = -maxAngle;
-            }
-        }
+    public void BoostAngle(float _angleBoost) {        
+        transform.Rotate(Vector3.back * _angleBoost);
     }
 }
