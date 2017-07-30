31c60566f87e92983033ca60f182cc79b62024df
Yakka <Yakka@users.noreply.github.com>
2017-07-30 17:08:46 +0200

    Generating the dick.

--- HarderStronger/Assets/Scripts/Dick.cs
+++ HarderStronger/Assets/Scripts/Dick.cs
@@ -3,5 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dick : MonoBehaviour {
+
+    public Section sectionModel = null;
+    public const int amountOfSections = 10;
+    public const float gapBetweenTwoSections = 0.7f;
+
+    public void Start() {
+        Transform parent = transform;
+
+        for(int i = 0; i < amountOfSections; i++) {
+            Section section = Instantiate(sectionModel);
+            section.transform.parent = parent;
+            section.name = "Section (" + i + ")";
+            section.transform.Translate(Vector3.left * gapBetweenTwoSections * i);
+            if(i == amountOfSections - 1) {
+                section.isHead = true;
+            } else {
+                parent = section.transform;
+            }
+        }
+    }
 }
 
--- HarderStronger/Assets/Scripts/Section.cs
+++ HarderStronger/Assets/Scripts/Section.cs
@@ -7,8 +7,14 @@ public class Section : MonoBehaviour {
     public const float maxAngle = 45f;
     public const float angleBoost = 5f;
 
+    public bool isHead = false;
+    public Sprite headSprite = null;
+
     // Use this for initialization
     void Start () {
+        if(isHead) {
+            GetComponent<SpriteRenderer>().sprite = headSprite;
+        }
 	}
 	
 	// Update is called once per frame
@@ -18,10 +24,21 @@ public class Section : MonoBehaviour {
             if (hit.collider.name == name) {
                 BoostAngle(angleBoost);
             }
+        } else if(Input.GetMouseButtonDown(1) && hit.collider != null) {
+            if (hit.collider.name == name) {
+                BoostAngle(-angleBoost);
+            }
         }
     }
 
     public void BoostAngle(float _angleBoost) {        
         transform.Rotate(Vector3.back * _angleBoost);
+
+        Section sectionChild = GetComponentInChildren<Section>();
+
+        if(sectionChild != null) {
+            //sectionChild.BoostAngle(-_angleBoost / 2f);
+        }
+
     }
 }
